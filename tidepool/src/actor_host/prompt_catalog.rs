266c95a23db7be74512d05a44a39542b3f919ef5
Inanna Malick <inanna@recursion.wtf>
2026-09-05 18:37:28 -0700

    Advance integrated Shoal prompt catalog

--- tidepool/src/actor_host/prompt_catalog.rs
+++ tidepool/src/actor_host/prompt_catalog.rs
@@ -10,7 +10,7 @@ pub(super) enum PromptId {
 }
 
 impl PromptId {
-    pub(super) const CATALOG_VERSION: u32 = 5;
+    pub(super) const CATALOG_VERSION: u32 = 6;
 
     #[cfg(test)]
     pub(super) const ALL: [Self; 7] = [
