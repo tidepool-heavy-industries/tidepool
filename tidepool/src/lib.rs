00209dbdc54ea6ce857946d3b7bfe5fc4ac2f673
Inanna Malick <inanna@recursion.wtf>
2026-09-06 12:28:14 -0700

    Add partial bounded run-map reader and retain custody evidence

--- tidepool/src/lib.rs
+++ tidepool/src/lib.rs
@@ -16,9 +16,9 @@
 #![warn(clippy::unwrap_used, clippy::expect_used)]
 pub mod actor_host;
 pub mod compile_report;
-pub mod run_map;
 pub mod haskell_sources;
 mod host_dynamic_tools;
+pub mod run_map;
 pub mod shoal;
 pub use tidepool_bridge as bridge;
 pub use tidepool_bridge_derive as bridge_derive;
