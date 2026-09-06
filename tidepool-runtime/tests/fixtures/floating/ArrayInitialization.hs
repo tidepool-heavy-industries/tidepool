84a28d31826f993876e280ef8db57e71634b6d2d
Inanna Malick <inanna@recursion.wtf>
2026-09-06 02:59:50 -0700

    Pin native array initialization and finite floatToDigits regressions

---
+++ tidepool-runtime/tests/fixtures/floating/ArrayInitialization.hs
@@ -0,0 +1,51 @@
+module ArrayInitialization where
+
+import Data.Array (Array, (!), array, listArray)
+import Data.Array.ST (newArray, runSTArray, writeArray)
+import Numeric (floatToDigits)
+
+{-# NOINLINE singleton #-}
+singleton :: Int -> Array Int Int
+singleton x = listArray (0,0) [x]
+
+{-# NOINLINE listed #-}
+listed :: Int -> Array Int Int
+listed x = listArray (0,2) [x, x+1, x+2]
+
+{-# NOINLINE associated #-}
+associated :: Int -> Array Int Int
+associated x = array (0,2) [(2,x+2),(0,x),(1,x+1)]
+
+{-# NOINLINE initialized #-}
+initialized :: Int -> Array Int Int
+initialized x = runSTArray (newArray (0,2) x)
+
+{-# NOINLINE written #-}
+written :: Int -> Array Int Int
+written x = runSTArray $ do
+  a <- newArray (0,2) (error "untouched array initializer")
+  writeArray a 1 x
+  pure a
+
+{-# NOINLINE looped #-}
+looped :: Int -> Array Int Int
+looped x = runSTArray $ do
+  a <- newArray (0,2) (error "unwritten loop cell")
+  mapM_ (\i -> writeArray a i (x+i)) [0..2]
+  pure a
+
+singleResult, listResult, associationResult, initialResult, writeResult, loopResult :: String
+singleResult = show (singleton 7 ! 0)
+listResult = show [listed 7 ! i | i <- [0..2]]
+associationResult = show [associated 7 ! i | i <- [0..2]]
+initialResult = show [initialized 7 ! i | i <- [0..2]]
+writeResult = show (written 7 ! 1)
+loopResult = show [looped 7 ! i | i <- [0..2]]
+
+{-# NOINLINE digits #-}
+digits :: Double -> ([Int],Int)
+digits = floatToDigits 10
+
+digitsResult, finiteShowResult :: String
+digitsResult = show (digits 1)
+finiteShowResult = show (1 :: Double)
